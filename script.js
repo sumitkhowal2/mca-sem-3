886727d269aabc79433acd030f2200c1b4394d9b
sumit khowal <119174462+sumitkhowal2@users.noreply.github.com>
2024-04-02 13:49:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function displayLoginComponent() {
-    var loginComponent = document.getElementById('login');
-    if (loginComponent) {
-        loginComponent.style.display = 'block';
-    }
-}
-setTimeout(displayLoginComponent, 3000);
